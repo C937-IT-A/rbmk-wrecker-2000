816b3dfdd1f0ae3ff17a5d779971ab76475ecaea
pumpjack <aetsofficialact@gmail.com>
2026-09-05 10:10:05 -0400

    timing fix

--- rbmk.lua
+++ rbmk.lua
@@ -662,7 +662,7 @@ threading.create(function()
     repeat
         for _, v in pairs(inpFunctions) do
             if not active then break end
-            os.sleep()
+            os.sleep(.05)
             v()
         end
     until not active
